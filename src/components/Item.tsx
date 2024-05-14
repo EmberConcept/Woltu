2f34f6d72f5f34b795d2dc2f677d1ccddbcfa35b
EmberConcept <f.tehrani@ember-concept.com>
2024-05-14 20:59:41 +0200

    UPDATED FOOTER

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 
-const Item = ({ Links, title }) => {
-    return (
-        <ul>
-            <h1 className="mb-1 font-semibold">{title}</h1>
-            {Links.map((link) => (
-                <li key={link.name}>
-                    <a
-                        className="text-gray-400 hover:text-gray-100 duration-300
-          text-sm cursor-pointer leading-6"
-                        href={link.link}
-                    >
-                        {link.name}
-                    </a>
-                </li>
-            ))}
-        </ul>
-    );
+interface Link {
+  name: string;
+  link: string;
+}
+
+interface Props {
+  title: string;
+  Links: Link[];
+}
+
+const Item: React.FC<Props> = ({ Links, title }) => {
+  return (
+    <ul>
+      <h1 className="mb-1 font-semibold">{title}</h1>
+      {Links.map((link) => (
+        <li key={link.name}>
+          <a
+            className="text-gray-400 hover:text-gray-100 duration-300 text-sm cursor-pointer leading-6"
+            href={link.link}
+          >
+            {link.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Item;

d10165252ce8b0820a6933115d81dedb8eda4a4f
EmberConcept <f.tehrani@ember-concept.com>
2024-05-15 08:54:15 +0200

    UPDATED LAYOUT.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Woltu Homepage",
+  title: "Woltu Energy Homepage",
   description: "Willkommen auf der Woltu ENERGY Homepage, ein Ort für grüne Photovoltaik-Innovation.",
 };
 
